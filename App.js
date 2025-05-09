76c86686d7aec715a8888b1d00dc8d05cb3f1560
Android Studio <devandroid@ort>
2025-05-09 10:38:48 -0300

    nuevo

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, TextInput, ImageBackground} from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, ImageBackground, TouchableOpacity} from 'react-native';
 const ImagenFondo = {uri: 'https://i.pinimg.com/736x/89/3b/52/893b52dc9efa9306b40394ed3206e574.jpg'}
 const FotoPerfilSantiago = {uri: 'https://cdn-icons-png.flaticon.com/512/1785/1785911.png'}
 export default function App() {
   return (
   <SafeAreaView style={styles.container} >
-  <View style={styles.tarjeta}>
-    <Text>Santiago Varela</Text>
-    <Text>Desarrollador Web</Text>
-    <TextInput style={styles.input} placeholder="Ingrese un mensaje" keyboardType="default"/> 
+    <View style={styles.tarjeta}>
+      <Text>Santiago Varela</Text>
+      <Text>Desarrollador Web</Text>
+      <TextInput style={styles.input} placeholder="Ingrese un mensaje" keyboardType="default"/> 
+      <TouchableOpacity><Text>Contactar</Text></TouchableOpacity>
+      <TouchableOpacity><Text>Ver Portfolio</Text></TouchableOpacity>
+
     </View>
   </SafeAreaView>
   );
